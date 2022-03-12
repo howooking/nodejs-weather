9f71d7b1f86e91dddd2268bf7abfb97a5509ae25
howooking <junsgk@gmail.com>
2022-03-12 21:31:54 +0900

    한글화

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
       callback('Please specify a valid location identifier using the query parameter.', undefined)
     } else {
       const data = body.current;
-      callback(undefined, `${data.weather_descriptions[0]}. It is currently ${data.temperature} degress out. It feels like ${data.feelslike} degress out.`)
+      callback(undefined, `${data.weather_descriptions[0]}. 현재 기온은 ${data.temperature}도이며. 체감 온도는 ${data.feelslike}도입니다..`)
     }
   })
 }
